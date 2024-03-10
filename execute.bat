efdb5deed957caa550562f05c8ef14d92ae99479
matheusangelo <matheusangelo10@gmail.com>
2024-03-10 20:29:58 -0300

    plugins

--- execute.bat
+++ execute.bat
@@ -1,6 +1,7 @@
-SET AWS_ACCESS_KEY_ID='test'
-SET AWS_SECRET_ACCESS_KEY='test'
-SET AWS_DEFAULT_REGION='us-east-1'
-SET AWS_ENDPOINT_URL=http://localhost:4566
+SET AWS_ACCESS_KEY_ID=S3RVER
+SET AWS_SECRET_ACCESS_KEY=S3RVER
+SET AWS_DEFAULT_REGION=us-east-1
+SET AWS_IGNORE_CONFIGURED_ENDPOINT_URLS=true
+@REM aws dynamodb create-table --table-name ddb --attribute-definitions AttributeName=Id,AttributeType=N --key-schema AttributeName=Id,KeyType=HASH --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5 --endpoint-url http://localhost:5008
 
 serverless offline start
--- handler.py
+++ handler.py
@@ -1,28 +1,41 @@
 import json
 import boto3
+import os
 from elasticsearch import Elasticsearch
 
-s3 = boto3.client('s3')
+s3 = boto3.client('s3', endpoint_url='http://localhost:4569')
+dynamodb = boto3.resource('dynamodb', endpoint_url='http://localhost:8000')
+
+TABLE_NAME = 'ddb'
 S3_BUCKET_NAME = 'bucket'
 key = 'data.json'
 es_host = 'localhost'
 es_port = 9200
 session = boto3.Session()
 credentials = session.get_credentials()
+table = dynamodb.Table(TABLE_NAME)
 
 es = Elasticsearch('localhost:9200')
 
 def create(event, context):
-
+    print(os.getenv('MY_ENV'))
     json_data = json.dumps(event['body'])
 
     es.index(index='people', doc_type='person', id=1, body=event['body'])
 
     s3.put_object(Bucket=S3_BUCKET_NAME, Key='data.json', Body=json_data) 
+    response = table.put_item(Item={
+        "Id": 1
+    })
 
+    # if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+    #     print("Item saved successfully.")
+    # else:
+    #     print("Error occurred while saving item.")
+    
     return {
         "statusCode": 200,
-        "body": json_data
+        "body": 'ok'
     }
   
 def get(event, context):
@@ -39,4 +52,4 @@ def get(event, context):
         "body": json.dumps(search_result['hits']['hits'])
     }
     
-    return response
+    return data
